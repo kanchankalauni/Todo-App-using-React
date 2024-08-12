81a3ad1275b5485e8937f119600e2323a8fdb7bd
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-08-12 22:09:52 +0530

    Todo added in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react"
+
 function App() {
-  
-  return (
-    <h1>hello</h1>
-  )
+
+    const [allTodos, setAllTodos] = useState([])
+    const [singleTodo, setSingleTodo] = useState({title : "", desc : ""})
+
+    function handleAddTodo() {
+        // setAllTodos(prevValue => [...prevValue, singleTodo])
+        setAllTodos([...allTodos, singleTodo])
+    }
+
+    return (
+        <>
+            <div>
+                <h1>Todo App</h1>
+                <input type="text" placeholder="title" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, title : e.target.value}))}/>
+                <br />
+                <br />
+                <input type="text" placeholder="description" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, desc : e.target.value}))}/>
+                <br />
+                <br />
+                <button onClick={handleAddTodo}>Add Todo</button>
+            </div>
+            <div>
+                {
+                    allTodos.map((data, i) => (
+                        <div key={i} style={{backgroundColor : "lightgray"}}>
+                            <p>{i+1}</p>
+                            <h1>{data.title}</h1>
+                            <p>{data.desc}</p>
+                            <button>Edit</button>
+                            <button>Remove</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
 
 export default App
