b7a2856c08bd65c0c10d91cab491eca0fa70719c
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-08-22 01:04:12 +0530

    Implemented Edit functionality in UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,39 @@ import { useEffect, useState } from "react"
 
 function App() {
 
+    const [isEditing, setIsEditing] = useState(false)
+    const [updatedIndex, setUpdatedIndex] = useState(null)
     const [allTodos, setAllTodos] = useState([])
     const [singleTodo, setSingleTodo] = useState({title : "", desc : ""})
 
     function handleAddTodo() {
         // setAllTodos(prevValue => [...prevValue, singleTodo])
-        setAllTodos([...allTodos, singleTodo])
-        saveTodoLocalStore([...allTodos, singleTodo])
+        if (isEditing) {
+            const updatedTodo = allTodos.map((todo,i) => updatedIndex == i ? singleTodo : todo)
+            setAllTodos([...updatedTodo])
+            isEditing(false)
+            setUpdatedIndex(null)
+        } else {
+            setAllTodos([...allTodos, singleTodo])
+            saveTodoLocalStore([...allTodos, singleTodo])
+        }
     }
 
     function deleteTodo(i) {
         let newArr = [...allTodos]              //Delete todo from UI
         newArr.splice(i,1)
-        saveTodoLocalStore(newArr)              //Updated Delete todo to Local Storage(Mothod 1)
+        saveTodoLocalStore(newArr)              //Updated, Delete todo to Local Storage(Mothod 1)
         setAllTodos(newArr)
 
         // allTodos.splice(i,1)                 //Delete todo from Local Storage(Methoda 2)
         // saveTodoLocalStore(allTodos)
         // getTodoFromLocalStore()
+    }
+
+    function editTodo(i) {
+        setSingleTodo(allTodos[i])
+        setIsEditing(true)
+        setUpdatedIndex(i)
     }
 
     function saveTodoLocalStore(todo) {
@@ -39,13 +54,13 @@ function App() {
         <>
             <div>
                 <h1>Todo App</h1>
-                <input type="text" placeholder="title" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, title : e.target.value}))}/>
+                <input type="text" value={singleTodo.title} placeholder="title" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, title : e.target.value}))}/>
                 <br />
                 <br />
-                <input type="text" placeholder="description" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, desc : e.target.value}))}/>
+                <input type="text" value={singleTodo.desc} placeholder="description" onChange={(e) => setSingleTodo(prevValue => ({...prevValue, desc : e.target.value}))}/>
                 <br />
                 <br />
-                <button onClick={handleAddTodo}>Add Todo</button>
+                <button onClick={handleAddTodo}>{isEditing ? "Update Todo" : "Add Todo"}</button>
             </div>
             <div>
                 {
@@ -54,7 +69,7 @@ function App() {
                             <p>{i+1}</p>
                             <h1>{data.title}</h1>
                             <p>{data.desc}</p>
-                            <button>Edit</button>
+                            <button onClick={() => editTodo(i)}>Edit</button>
                             <button onClick={() => deleteTodo(i)}>Remove</button>
                         </div>
                     ))
